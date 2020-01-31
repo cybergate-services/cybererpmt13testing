1405965414df14c61edb5635d2e28c9b8ad611ff
DamithDDS <32153823+DamithDDS@users.noreply.github.com>
2020-01-31 09:31:19 +0530

    Update backup_restore.sh

--- scripts/backup_restore.sh
+++ scripts/backup_restore.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-lbackuplocpost=/opt/cybererp/postgresql_data
-lbackuplocodoo=/opt/cybererp/odoo_data
+lbackuplocpost=/opt/cybererpmt/postgresql*
+lbackuplocodoo=/opt/cybererpmt/odoo_data
 
 rbackuploc=/opt/backup/
 DATE=`date +%Y%m%d%H%M`
@@ -8,8 +8,8 @@ DATE=`date +%Y%m%d%H%M`
 #rsync -avp $lbackuplocpost :$rbackuploc
 #rsync -avp $lbackuplocodoo :$rbackuploc
 
-rsync -avp /opt/cybererp/postgresql_data /opt/backup/$DATE
-rsync -avp /opt/cybererp/odoo_data /opt/backup/$DATE
+rsync -avp /opt/cybererpmt/postgresql* /opt/backup/$DATE
+rsync -avp /opt/cybererpmt/odoo_data /opt/backup/$DATE
 
 
 #find $rbackuploc/* -mtime +10 -exec rm {} \;
